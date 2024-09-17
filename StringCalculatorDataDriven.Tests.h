5b019b23299e92db27ae542fa2f1a89d2eb67f91
BhavaniMaddela <maddelab45@gmail.com>
2024-09-17 17:08:14 +0530

    Create StringCalculatorDataDriven.Tests.h

---
+++ StringCalculatorDataDriven.Tests.h
@@ -0,0 +1,30 @@
+#include "StringCalculator.h"
+#include <gtest/gtest.h>
+
+class TestDataPair{
+
+public:
+   string input;
+   int expectedValue
+  TestDataPair(String_input,int_expectedValue) : input{_input}, expectedValue{_expectedValue}{
+   }
+};
+
+TEST(StringCalculatorDataDrivenTestSuite,DataDrivenTestCase){
+  vector<TestDataPair> datalist;
+  TestDataPair Pair_one{"",0};
+  TestDataPair Pair_two{"0",0};
+  TestDataPair Pair_three{"1",1};
+  TestDataPair Pair_four{"1,2",3};
+  TestDataPair Pair_five{"1,2,3",6};
+
+dataList.push_back(pair_one);
+dataList.push_back(pair_two);
+dataList.push_back(pair_three);
+dataList.push_back(pair_four);
+dataList.push_back(pair_five);
+
+/iterate using a range-based for-loop
+  for(TestDataPair DataPair : datalist){
+   int actualValue=Add(datapair,input);
+ASSERT_EQ(actualValue,datapair.expectedValue);
