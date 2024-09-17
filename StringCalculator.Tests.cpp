9327e2dccece52c812eaf08c05daed6c0ff5dc1d
BhavaniMaddela <maddelab45@gmail.com>
2024-09-17 17:32:32 +0530

    Update StringCalculator.Tests.cpp

--- StringCalculator.Tests.cpp
+++ StringCalculator.Tests.cpp
@@ -3,123 +3,36 @@
 
 class StringCalculatorFixture:public testing::Test{
 protected:
-string input;
-int expectedValue;
-int actualValue;
+    string input;
+    int expectedValue;
+    int actualValue;
 };
 
-TEST_F(StringCalculatorTestSuite,add_emptyInputString_ZeroIsExpected){
-//Arrange
-input="";
-expectedValue=0;
-//Act
-actualValue=Add(input);
-//Assert
-ASSERT_EQ(actualValue,expectedValue);
- 
+TEST_F(StringCalculatorFixture,add_emptyInputString_ZeroIsExpected){
+  //Arrange
+   input="";
+  expectedValue=0;
+  //Act
+  actualValue=Add(input);
+  //Assert
+  ASSERT_EQ(actualValue,expectedValue);
 }
 
-TEST(string_calculatorTestSuite,add_ZeroInputString_ZeroIsExpected){
-//Arrange
-string input="0";
-int expectedValue=0;
-//Act
-int actualValue=Add(input);
-//Assert
-ASSERT_EQ(actualValue,expectedValue);
- 
+TEST(StringCalculatorTestSuite,add_emptyInputString_ZeroIsExpected){
+  //Arrange
+  string input="";
+  int expectedValue=0;
+  //Act
+  int actualValue=Add(input);
+  //Assert
+  ASSERT_EQ(actualValue,expectedValue);
 }
-
-TEST(string_calculatorTestSuite,add_two_comma_delimited_numbersString_SumIsExpected){
-//Arrange
-string input="1,2";
-int expectedValue=3;
-//Act
-int actualValue=Add(input);
-//Assert
-ASSERT_EQ(actualValue,expectedValue);
- 
+TEST(StringCalculatorTestSuite,add_InputZero_ZeroIsExpected){
+  //Arrange
+  string input="0";
+  int expectedValue=0;
+  //Act
+  int actualValue=Add(input);
+  //Assert
+  ASSERT_EQ(actualValue,expectedValue);
 }
-
-TEST(string_calculatorTestSuite,add_multiple_comma_delimited_numbersString_SumIsExpected){
-//Arrange
-string input="1,2,7";
-int expectedValue=10;
-//Act
-int actualValue=Add(input);
-//Assert
-ASSERT_EQ(actualValue,expectedValue);
- 
-}
-TEST(string_calculatorTestSuite,passed_a_delimiterString_SumIsExpected){
-//Arrange
-string input="//;\n1;2";
-int expectedValue=3;
-//Act
-int actualValue=Add(input);
-//Assert
-ASSERT_EQ(actualValue,expectedValue);
- 
-}
-TEST(string_calculatorTestSuite,when_delimited_with_newline_and_commaString_SumIsExpected){
-//Arrange
-string input="1\n2,3";
-int expectedValue=6;
-//Act
-int actualValue=Add(input);
-//Assert
-ASSERT_EQ(actualValue,expectedValue);
- 
-}
-TEST(string_calculatorTestSuite,when_passed_multicharacter_delimiterString_SumIsExpected){
-//Arrange
-string input="//[***]\n8***2***3";
-int expectedValue=13;
-//Act
-int actualValue=Add(input);
-//Assert
-ASSERT_EQ(actualValue,expectedValue);
-
-}
-
-TEST(string_calculatorTestSuite,passed_multiple_delimitersString_SumIsExpected){
-//Arrange
-string input="//[*][%]\n4*2%3";
-int expectedValue=9;
-//Act
-int actualValue=Add(input);
-//Assert
-ASSERT_EQ(actualValue,expectedValue);
- 
- 
-}
-TEST(string_calculatorTestSuite,passed_multiple_multicharacter_delimitersString_SumIsExpected){
-//Arrange
-string input="//[**][%^]\n4**1%^9";
-int expectedValue=14;
-//Act
-int actualValue=Add(input);
-//Assert
-ASSERT_EQ(actualValue,expectedValue);
- 
-}
-TEST(string_calculatorTestSuite,when_passed_numbers_over_1000String_SumIsExpected){
-//Arrange
-string input="//46,1001,3";
-int expectedValue=49;
-//Act
-int actualValue=Add(input);
-//Assert
-ASSERT_EQ(actualValue,expectedValue);
- 
-}
-TEST(string_calculatorTestSuite,add_Negative_numbersString_InvalidargumentIsExpected){
-//Arrange
-string input="-3";
-//Act
-
-//Assert
-ASSERT_THROW(Add(input),invalid_argument);
-
-}
- 
