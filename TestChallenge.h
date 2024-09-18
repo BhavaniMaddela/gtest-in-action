7ecf0d7ed675ea06622ab7c9eb945f278d9b0c1c
BhavaniMaddela <maddelab45@gmail.com>
2024-09-18 16:57:16 +0530

    Update TestChallenge.h

--- TestChallenge.h
+++ TestChallenge.h
@@ -1,2 +1,11 @@
-void alertInCelcius(float farenheit);
-int getFailureCount();
+//void alertInCelcius(float farenheit);
+//int getFailureCount();
+
+//Abstraction using Class Interface
+// functionpointer
+class INetworkAlerter{
+public:
+    virtual void alert(float celcius)=0;
+};
+
+int alertInCelciusFacade(float farenheit,INetworkAlerter *networkAlerterPtr);
