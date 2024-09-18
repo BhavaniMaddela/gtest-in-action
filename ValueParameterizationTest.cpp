0347a50044001ccb0d44f2953f6e9ece1ab791eb
BhavaniMaddela <maddelab45@gmail.com>
2024-09-18 11:33:32 +0530

    Update ValueParameterizationTest.cpp

--- ValueParameterizationTest.cpp
+++ ValueParameterizationTest.cpp
@@ -8,21 +8,25 @@ protected:
     int actualValue;
 };
 class StringCalculatorParameterFixture:public StringCalculatorFixture,public testing::withparaminterface<tuple<string,int>>{
+   void SetUp() override {
+        input = std::get<0>(GetParam());
+        expectedValue = std::get<1>(GetParam());
 };
+
 //paramater values
 //INSTANTIATE_TEST_SUITE_P(name of collection,fixture with test param, testing::Values(make_tuple()));
 INSTANTIATE_TEST_SUITE_P(ValidStringCalculatorInputs,StringCalculatorParameterFixture,testing::values(
   make_tuple("",0)
   make_tuple("0", 0),
   make_tuple("1", 1),
-  make_tuple("1,4", 3),
+  make_tuple("1,2", 3),
   make_tuple("1,2,3", 6)
   
 ));
 
 TEST_P(StringCalculatorParameterFixture,ParameterizedTest){
-      input= std::get<0>(GetParam());
-      expectedValue= std::get<1>(GetParam());
+      //input= std::get<0>(GetParam());
+      //expectedValue= std::get<1>(GetParam());
       actualValue=Add(input);
       ASSERT_EQ(actualValue,expectedValue);
 }
