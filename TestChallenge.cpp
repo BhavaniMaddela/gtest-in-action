92b6d29538b226bb8d900ac2d6397121b1bb8ca8
BhavaniMaddela <maddelab45@gmail.com>
2024-09-18 17:17:38 +0530

    Update TestChallenge.cpp

--- TestChallenge.cpp
+++ TestChallenge.cpp
@@ -3,9 +3,11 @@
 #include "TestChallenge.h"
  
 int alertFailureCount = 0;
+
 int getFailureCount(){
 return alertFailureCount;
 }
+
 void alertInCelcius(float farenheit,INetworkAlerter *networkAlerterPtr) {
     float celcius = (farenheit - 32) * 5 / 9;
     int returnCode = networkAlerterPtr->alert(celcius);
@@ -18,3 +20,4 @@ int alertInCelciusFacade(float farenheit,INetworkAlerter *networkAlerterPtr){
    alertInCelcius(farenheit,networkAlerterPtr);
    return getFailureCount();
  }
+ 
