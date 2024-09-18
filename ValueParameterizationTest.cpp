b467bfbcfb353c46ac7c13a40c5115816536c0ee
BhavaniMaddela <maddelab45@gmail.com>
2024-09-18 12:53:28 +0530

    Update ValueParameterizationTest.cpp

--- ValueParameterizationTest.cpp
+++ ValueParameterizationTest.cpp
@@ -3,20 +3,22 @@
 
 class StringCalculatorFixture:public testing::Test{
 protected:
-    string input;
-    int expectedValue;
-    int actualValue;
-};
-class StringCalculatorParameterFixture:public StringCalculatorFixture,public testing::withparaminterface<tuple<string,int>>{
-   void SetUp() override {
-        input = std::get<0>(GetParam());
-        expectedValue = std::get<1>(GetParam());
+   string input;
+   int expectedValue;
+   int actualValue;
 };
 
-//paramater values
-//INSTANTIATE_TEST_SUITE_P(name of collection,fixture with test param, testing::Values(make_tuple()));
-INSTANTIATE_TEST_SUITE_P(ValidStringCalculatorInputs,StringCalculatorParameterFixture,testing::values(
-  make_tuple("",0)
+class StringCalculatorParameterFixture:public StringCalculatorFixture,public testing::WithParamInterface<tuple<string,int>>{
+    void SetUp() override {
+        input = std::get<0>(GetParam());
+        expectedValue = std::get<1>(GetParam());
+    }
+};
+
+//Parameter Values
+//INSTANTIATE_TEST_SUITE_P(name, fixture, values)
+INSTANTIATE_TEST_SUITE_P(ValidStringCalculatorInputs,StringCalculatorParameterFixture,testing::Values(
+  make_tuple("", 0),
   make_tuple("0", 0),
   make_tuple("1", 1),
   make_tuple("1,2", 3),
@@ -24,11 +26,9 @@ INSTANTIATE_TEST_SUITE_P(ValidStringCalculatorInputs,StringCalculatorParameterFi
   
 ));
 
+//TEST_P(Fixture,testcase)
 TEST_P(StringCalculatorParameterFixture,ParameterizedTest){
-     // input= std::get<0>(GetParam());
-      //expectedValue= std::get<1>(GetParam());
-    Add(input);
-      //actualValue=Add(input);
-      //ASSERT_EQ(actualValue,expectedValue);
+   Add(input);
+     // actualValue=Add(input);
+     // ASSERT_EQ(actualValue,expectedValue);
 }
-);
